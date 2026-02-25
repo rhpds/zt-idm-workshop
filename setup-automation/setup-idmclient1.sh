a63e1ef1da694e13191f22e18cd76835d14631f2
parmstro <parmstro@redhat.com>
2026-02-24 20:37:31 -0500

    update to add cockpit webui

--- setup-automation/setup-idmclient1.sh
+++ setup-automation/setup-idmclient1.sh
@@ -21,7 +21,7 @@ export SUBDOMAIN=lab.sandbox-${GUID}-zt-rhelbu.svc.cluster.local
 
 # Enable cockpit functionality in showroom.
 echo "[WebService]" > /etc/cockpit/cockpit.conf
-echo "Origins = https://${IDM_CLIENT1_NAME}" >> /etc/cockpit/cockpit.conf
+echo "Origins = https://idmclient1-${GUID}.${DOMAIN}" >> /etc/cockpit/cockpit.conf
 echo "AllowUnencrypted = true" >> /etc/cockpit/cockpit.conf
 systemctl enable --now cockpit.socket
 
